0024908f78231e9ca92227c5586bd893e51b0a25
obylo98 <obinnaandrewonyekwere@gmail.com>
2024-07-23 18:13:14 +0000

    added footer and updated the header

--- project/scripts/portfolio.js
+++ project/scripts/portfolio.js
@@ -7,3 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
         navUL.classList.toggle("active");
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const currentYear = new Date().getFullYear();
+
+    const lastModified = document.lastModified;
+
+    document.getElementById('currentYear').textContent = currentYear;
+
+    document.getElementById('lastModified').textContent = lastModified;
+});
